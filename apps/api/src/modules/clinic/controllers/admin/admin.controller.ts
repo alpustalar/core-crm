756792259227b542ee71338193754f52223df04c
alpustalar <131296202+alpustalar@users.noreply.github.com>
2026-03-03 19:34:32 +0300

    dosya yapısı değiştirildi

---
+++ apps/api/src/modules/clinic/controllers/admin/admin.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Delete,
+  Param,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { SoftDeleteClinicUseCase } from '@modules/clinic/use-cases';
+import { AuthGuard } from '@common/guards';
+import { SystemAdminGuard } from '@common/guards/system-admin/system-admin.guard';
+
+@UseGuards(AuthGuard, SystemAdminGuard)
+@Controller('admin')
+export class AdminController {
+  constructor(
+    private readonly SoftDeleteClinicUseCase: SoftDeleteClinicUseCase
+  ) {}
+
+  @Delete(':clinicId')
+  softDelete(@Param('clinicId', ParseUUIDPipe) clinicId: string) {
+    return this.SoftDeleteClinicUseCase.execute(clinicId);
+  }
+}
--- shared/modules/organization/types/create-organization.type.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-import { CreateOrganizationSchema } from "@shared/modules/organization";
-
-export type CreateOrganization = z.infer<typeof CreateOrganizationSchema>;
